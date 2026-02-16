379f3f1672648bac797338aeb9700ce797bfb0d6
bissak <baglan.issak@gmail.com>
2026-02-16 21:52:33 +0500

    L6 Added connecting to db via JDBC and JPA.

--- payment-service-app/src/main/java/kz/iprody/paymentserviceapp/controller/PaymentController.java
+++ payment-service-app/src/main/java/kz/iprody/paymentserviceapp/controller/PaymentController.java
@@ -1,36 +1,30 @@
 package kz.iprody.paymentserviceapp.controller;
 
-import kz.iprody.paymentserviceapp.model.Payment;
+import kz.iprody.paymentserviceapp.persistence.entity.Payment;
+import kz.iprody.paymentserviceapp.persistence.entity.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/payments")
 public class PaymentController {
-
-    private final Payment payment = new Payment(1L, 99.99);
-    private Map<Long, Payment> payments = new HashMap<>();
-    public PaymentController() {
-        payments.put(1L, payment);
-        payments.put(2L, new Payment(2L, 150.50));
-        payments.put(3L, new Payment(3L, 200.00));
-        payments.put(4L, new Payment(4L, 75.25));
-        payments.put(5L, new Payment(5L, 300.99));
-    }
+    @Autowired
+    private PaymentRepository paymentRepository;
 
     @GetMapping
-    public ArrayList<Payment> getPayment() {
-        return new ArrayList<>(payments.values());
+    public List<Payment> getPayment() {
+        return paymentRepository.findAll();
     }
 
-    @GetMapping("/{userId}")
-    public Payment getPayments(@PathVariable Long userId) {
-        return payments.get(userId);
+    @GetMapping("/{id}")
+    public Optional<Payment> getPayments(@PathVariable UUID id) {
+        return paymentRepository.findById(id);
     }
 }
